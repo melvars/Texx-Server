efe4b98e6f3d5d63bc2da8110f474d6b7ec122df
Marvin Borner <marvin-borner@live.com>
2018-07-06 23:49:59 +0200

    Added public key database migration

---
+++ database/migrations/2018_07_06_213554_create_public_key_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicKeyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_keys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->unique();
+            $table->string('key', 4096);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_key');
+    }
+}
