ef3712cd7f0a4ccef324b68a4e0e078c657d98ab
Marvin Borner <marvin-borner@live.com>
2018-07-07 12:42:47 +0200

    Better http error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\View;
+use \Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof HttpException) {
+            $statusCode = $exception->getStatusCode();
+            return response(View::make('errors.http', [
+                'ErrorMessage' => $exception->getMessage() == '' ? 'An unknown error occurred.' : $exception->getMessage(),
+                'ErrorCode' => $statusCode
+            ]), $statusCode);
+        }
+
         return parent::render($request, $exception);
     }
 }
---
+++ resources/views/errors/http.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.error')
+
+@section('title', $ErrorMessage)
+
+@section('message', $ErrorMessage)
