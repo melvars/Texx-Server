87484e7221503f2eeeedd4a7a405eddc57ead95d
Marvin Borner <marvin-borner@live.com>
2018-07-07 16:23:01 +0200

    Fixed register key generation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button disabled type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
