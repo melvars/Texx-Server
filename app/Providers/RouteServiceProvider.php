b9ce852cd3d5531053c0e22d9ff1c6b31d7d791e
Marvin Borner <marvin-borner@live.com>
2018-07-09 20:01:35 +0200

    Throtteling for API

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -70,7 +70,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-        //->middleware('api')
+            ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
