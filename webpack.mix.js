82de5eb4e5f5349bd0b93b0f728827ae08818f89
Marvin Borner <marvin-borner@live.com>
2018-07-11 21:44:35 +0200

    Added browsersync for faster development/debug

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,16 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.disableNotifications();
-
 mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/initial_key_gen.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps()
+    .browserSync('http://127.0.0.1:8000', {
+        files: [
+            'public/css/*.css',
+            'public/js/*.js'
+        ],
+        injectChanges: true,
+        proxy: false
+    })
+    .disableNotifications();
