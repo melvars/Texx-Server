b8c219ed7a482740927e301502b9522c13eddeb1
Marvin Borner <marvin-borner@live.com>
2018-07-11 19:00:05 +0200

    Better server starting script

--- StartServer.sh
+++ StartServer.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-node resources/assets/js/chatServer.js
+#!/bin/bash -e
+redis-server &
+PIDS[0]=$!
+node resources/assets/js/chatServer.js &
+PIDS[1]=$!
+php artisan queue:work & 
+PIDS[2]=$!
+php artisan serve &
+PIDS[3]=$!
+npm run watch &
+PIDS[4]=$!
+
+trap "kill ${PIDS[*]}" SIGINT
+
+wait
