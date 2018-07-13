8dcf073b4ae5cc609cc6cadc854d60adaa0f5e5e
Marvin Borner <marvin-borner@live.com>
2018-07-13 22:59:43 +0200

    More beautiful routes :)

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ Route::get('verify/email/{token}', 'Auth\RegisterController@verifyEmail');
 Auth::routes();
 Route::middleware('auth', 'throttle:30') // throttle to 30 per minute
     ->group(function () {
-        Route::get('/', ['as' => 'writeMessage', 'uses' => 'SocketController@writeMessage']);
-        Route::get('avatar/{user_id}', 'ImageController@getAvatar');
-        Route::get('profile', 'UserController@Profile');
-        Route::get('admin', 'AdminController@Overview');
+        Route::get('/', ['as' => 'MessageChat', 'uses' => 'SocketController@writeMessage']);
+        Route::get('avatar/{user_id}', ['as' => 'GetAvatar', 'uses' => 'ImageController@getAvatar']);
+        Route::get('profile', ['as' => 'GetProfileSettings', 'uses' => 'UserController@Profile']);
+        Route::get('admin', ['as' => 'GetAdminOverview', 'uses' => 'AdminController@Overview']);
 
-        Route::post('avatar', 'UserController@updateAvatar');
-        Route::post('sendMessage', 'SocketController@sendMessage');
+        Route::post('avatar', ['as' => 'UpdateAvatar', 'uses' => 'UserController@updateAvatar']);
+        Route::post('sendMessage', ['as' => 'SendMessage', 'uses' => 'SocketController@sendMessage']);
     });
