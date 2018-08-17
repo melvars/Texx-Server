3045cb39be8b9c9cb3ca6ed643ccceac0042f0c0
Marvin Borner <marvin-borner@live.com>
2018-08-17 18:44:54 +0200

    Extended lifetime of token

--- infrastructure/Auth/AuthServiceProvider.php
+++ infrastructure/Auth/AuthServiceProvider.php
@@ -34,7 +34,7 @@ class AuthServiceProvider extends ServiceProvider
             $router->forTransientTokens();
         });
 
-        Passport::tokensExpireIn(Carbon::now()->addDays(365));
+        Passport::tokensExpireIn(Carbon::now()->addYears(10));
 
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(365));
     }
