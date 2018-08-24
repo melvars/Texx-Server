1362cdab92ffd8b8f375f42886e34a6674f2b5af
Marvin Borner <marvin-borner@live.com>
2018-08-24 16:19:21 +0200

    Fixed wrong class name

--- database/migrations/2018_08_23_231531_create_text_posts_table.php
+++ database/migrations/2018_08_23_231531_create_text_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTextTable extends Migration
+class CreateTextPostsTable extends Migration
 {
     /**
      * Run the migrations.
