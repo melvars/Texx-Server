06031351cf00986e7c2118ddcf892fa1c7e7234e
Marvin Borner <marvin-borner@live.com>
2018-08-27 00:47:26 +0200

    Added basic websocket service

---
+++ infrastructure/Console/Commands/WebSocketServerCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Infrastructure\Console\Commands;
+
+use Illuminate\Console\Command;
+use Infrastructure\Http\WebSocketController;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+class WebSocketServerCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'websocket';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new WebSocketController()
+                )
+            ),
+            1337
+        );
+        $this->line('Started server.');
+        $server->run();
+    }
+}
