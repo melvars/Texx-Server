1309e462efc4a7bbdf538e9d209133c1b3c138a6
Marvin Borner <marvin-borner@live.com>
2018-10-25 22:22:55 +0200

    Added authentication and post endpoint

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,7 @@
+const posts = require('./posts/posts.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
+  app.configure(posts);
+  app.configure(users);
 };
